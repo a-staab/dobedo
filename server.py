725c98e717bddcff95cc1fd006bc9daa49d24f0b
a-staab <astaab@gmail.com>
2017-03-28 10:29:06 +0000

    Created stubs for routes in server.py and activity setup form in setup.html

---
+++ server.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, render_template
+
+@app.route("/")
+def show_landing_page():
+    """ DOCSTRING"""
+
+    # CODE
+
+    return render_template("landing.html")
+
+@app.route("/signin")
+def create_act_types():
+    """ DOCSTRING"""
+
+    # CODE
+
+    return # CODE
+
+
+@app.route("/setup...")
+def ______():
+    """ DOCSTRING"""
+
+    # CODE
+
+    return # CODE
+
+@app.route("/...")
+def ______():
+    """ DOCSTRING"""
+
+    # CODE
+
+    return # CODE 
+
+
+@app.route("/...")
+def ______():
+    """ DOCSTRING"""
+
+    # CODE
+
+    return # CODE 
