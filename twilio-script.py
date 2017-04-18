1b018bc12431ee1bfc01c5480601e17e92dbb653
a-staab <astaab@gmail.com>
2017-04-18 23:08:40 +0000

    Crontab scheduled for 10:00 am daily and working. User with any planned activities with a start time of over 24 hours ago gets a reminder to (sign in and) finish their entry.

--- twilio-script.py
+++ twilio-script.py
@@ -1,16 +1,17 @@
 from model import db, Occurrence
 from twilio.rest import Client
 from flask import Flask
+from datetime import datetime, timedelta
+import os
+
 # schedule to run using crontab
 
 # for multiple commands, use && operator
 
-# env > env/bin/python twilio.python
+# env > env/bin/python twilio-script.py
 
 # Tell crontab to use bash, not sh:
-    # SHELL=/bin/bash
-
-# query for phone numbers of people with unfinished occurrences
+# SHELL=/bin/bash
 
 app = Flask(__name__)
 
@@ -19,26 +20,29 @@ db.app = app
 db.init_app(app)
 db.create_all()
 
+print "yes this ran"
+# Query for phone numbers of people with incompleted occurrences
 numbers_to_dial = set()
 incomplete_occurrences = Occurrence.query.filter((Occurrence.end_time.is_(None) | Occurrence.after_rating.is_(None))).all()
 
+# Create a list with the phone number for each user who has an incomplete
+# occurrence scheduled to have started over 24 hours ago. This is for an SMS
+# reminder to open the app and complete their entry, given that they should have
+# completed the activity and have had enough time to remember to document it at
+# this point (while not having, yet, forgotten how they felt about it).
 for occurrence in incomplete_occurrences:
-    if occurrence.activity.user.phone_number:
+    if occurrence.activity.user.phone_number and occurrence.start_time < (datetime.now() - timedelta(hours=24)):
         numbers_to_dial.add(occurrence.activity.user.phone_number)
 
-# Your Account SID from twilio.com/console
-account_sid = "ACae07e8d9c33fae8613b214c288ac637c"
-# Your Auth Token from twilio.com/console
-auth_token = "d49e0ff0fe2b2f738c6a50544e2b9a33"
+account_sid = os.environ["ACCOUNT_SID"]
+auth_token = os.environ["AUTH_TOKEN"]
 
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-    to="+15108330326",
-    from_="+19073121980",
-    body="Hi Amber! You're off to a great start!")
+for number in numbers_to_dial:
+    message = client.messages.create(
+        to=number,
+        from_="+19073121980",
+        body="""Hey friend! Looks like you've got one or more planned activities you might have finished. Let's hear how you felt, while it's still fresh in your mind!""")
 
-print(message.sid)
-
-# sqlalchemy homework - create an app (connect_to_db)
-# flash messages??
+    print(message.sid)
